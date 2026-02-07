669f8f233c33d49447d42b2876e1d6b174141555
Aniket Raj <araj52444@gmail.com>
2026-02-07 09:26:15 +0530

    feat: enhance user verification middleware with error handling and consistent response structure

--- midllewares/verify-user.js
+++ midllewares/verify-user.js
@@ -1,23 +1,33 @@
+import { verifyToken } from "../lib/utils.js";
+
 export const verifyUser = (req, res, next) => {
-  const authHeader = req.headers.Authorization;
-  if (!authHeader || !authHeader.includes("Bearer")) {
-    return res.status(401).json({
+  try {
+    const authHeader = req.headers.Authorization;
+    if (!authHeader || !authHeader.includes("Bearer")) {
+      return res.status(401).json({
+        success: false,
+        message: "Please login again",
+      });
+    }
+  
+    const token = authHeader.split(" ")[1];
+  
+    if (!token) {
+      return res.status(401).json({
+        success: false,
+        message: "Please login again",
+      });
+    }
+  
+    const payload = verifyToken(token);
+  
+    req.userId = payload.id;
+  
+    next();
+  } catch (error) {
+    res.status(500).json({
       success: false,
-      message: "Please login again",
-    });
+      error: `Error while verifying user: ${error.message}`,
+    })
   }
-
-  // const str = "Bearer token"
-
-  // str.split(" ") //["Bearer", "token"]
-
-  const token = authHeader.split(" ")[1];
-  if (!token) {
-    return res.status(401).json({
-      success: false,
-      message: "Please login again",
-    });
-  }
-//   req.userId = 
-
 };
