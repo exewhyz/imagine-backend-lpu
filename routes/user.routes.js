e9b628d55a0ca9b8edf8fe7229063ee5639720e5
Aniket Raj <araj52444@gmail.com>
2026-02-10 15:52:16 +0530

    fix: update user route to include verification middleware

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,10 +1,18 @@
-import { Router } from "express"
-import { deleteUser, updateUser, getAllUsers, getUser, login, register } from "../controllers/user.controller.js";
+import { Router } from "express";
+import {
+  deleteUser,
+  updateUser,
+  getAllUsers,
+  getUser,
+  login,
+  register,
+} from "../controllers/user.controller.js";
+import { verifyUser } from "../midllewares/verify-user.js";
 
 const userRouter = Router();
 
 userRouter.get("/users", getAllUsers);
-userRouter.get("/user/:id", getUser);
+userRouter.get("/user", verifyUser, getUser);
 userRouter.post("/register", register);
 userRouter.post("/login", login);
 userRouter.patch("/user/:id", updateUser);
