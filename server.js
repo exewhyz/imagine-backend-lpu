e5945fa68a4eae77dc1a599663b4d1dd5b897e86
Aniket Raj <araj52444@gmail.com>
2026-02-12 13:22:32 +0530

    feat: add CORS support to the server and update package dependencies

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
-import "dotenv/config"
+import "dotenv/config";
 
 import express from "express";
 import userRouter from "./routes/user.routes.js";
 import imageRouter from "./routes/image.routes.js";
 import { verifyUser } from "./midllewares/verify-user.js";
+
+import cors from "cors";
 
 import connectDB from "./lib/db.js";
 
@@ -12,6 +14,11 @@ const PORT = Number(process.env.PORT) || 4000;
 const app = express();
 
 app.use(express.json());
+app.use(
+  cors({
+    origin: "*",
+  }),
+);
 
 app.get("/", (req, res) => {
   res.status(200).json({
@@ -21,9 +28,9 @@ app.get("/", (req, res) => {
 });
 
 app.use("/api/auth", userRouter);
-app.use("/api/images",verifyUser, imageRouter);
+app.use("/api/images", verifyUser, imageRouter);
 
 app.listen(PORT, async () => {
-  await connectDB()
+  await connectDB();
   console.log(`Server is started on http://localhost:${PORT}`);
 });
